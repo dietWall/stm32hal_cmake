d09c33157c190be3d80c444f1a7bdb0a487ef5ac
Dietrich Wall <dietrich7@gmx.de>
2025-12-19 12:07:28 +0000

    cleanup of unused files
    Signed-off-by: Dietrich Wall <dietrich7@gmx.de>

--- Examples/simple_main/main.c
+++ Examples/simple_main/main.c
@@ -1,22 +1,6 @@
 
 
-#define STM32F767xx
-
 #include "stm32f7xx.h"
-
-
-void SystemInit(void)
-{
-  /* FPU settings ------------------------------------------------------------*/
-#if (__FPU_PRESENT == 1) && (__FPU_USED == 1)
-  SCB->CPACR |= ((3UL << 10*2)|(3UL << 11*2));  /* set CP10 and CP11 Full Access */
-#endif
-
-  /* Configure the Vector Table location -------------------------------------*/
-#if defined(USER_VECT_TAB_ADDRESS)
-  SCB->VTOR = VECT_TAB_BASE_ADDRESS | VECT_TAB_OFFSET; /* Vector Table Relocation in Internal SRAM */
-#endif /* USER_VECT_TAB_ADDRESS */
-}
 
 int main(void)
 {
