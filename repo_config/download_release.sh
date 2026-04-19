b9e9e02e744f1f649df4c7cde7056cc342a59402
Dietrich Wall <dietrich7@gmx.de>
2026-04-19 09:16:33 +0200

    finally added a proper script to download a private release

--- repo_config/download_release.sh
+++ repo_config/download_release.sh
@@ -24,22 +24,37 @@
 # If your version/tag doesn't match, the script will exit with error.
 
 #TOKEN="<github_access_token>"
-REPO="dietwall/openocd-tcl-controller"
-VERSION="latest"                       # tag name or the word "latest"
+
+DOWNLOAD_DIR=$(pwd)/repo_config/tmp_download    #this is adapted to this repo, change at will if reusing
+#REPO="dietwall/openocd-tcl-controller"          #this should be mandatory actually
+VERSION="latest"                                # tag name or the word "latest", tag is a git tag created at release
 GITHUB="https://api.github.com"
 
-while getopts "t:r:f:v:" opt; do
+while getopts "t:r:f:v:o:" opt; do
   case $opt in
     t) TOKEN="$OPTARG" ;;
     r) REPO="$OPTARG" ;;
     f) FILE="$OPTARG" ;;
     v) VERSION="$OPTARG" ;;
-    *) echo "Usage: $0 -r <organization/repo> [-t token] [-f file] [-v version]" >&2
+    o) DOWNLOAD_DIR="$OPTARG" ;;
+    *) echo "Usage: $0 -r <organization/repo> [-t token] [-f file] [-v version] [-o outdir]" >&2
+       echo "$0 -r dietwall/repo_helper -o \$(pwd)/repo_config/tmp_download"
+       echo "-r is mandatory"
+       echo "-t is a Github Private Token, if omited, the script will take a TOKEN environment variable"
+       echo "-f if provided, the file will be renamed to this arg, otherwise its name will be kept"
+       echo "-v latest if ommited, otherwise it should be exactly the git tag, that has been created at release"
+       echo "-o if ommited it will be current directory, otherwise the directory will be created"
        exit 1 ;;
   esac
 done
 
-alias errcho='>&2 echo'
+echo "Downloading from: $REPO"
+echo "Version: $VERSION"
+
+if [ -z $TOKEN ]; then
+  echo "environment variable TOKEN not set, cannot proceed"
+  exit 1
+fi;
 
 function gh_curl() {
   curl -H "Authorization: token $TOKEN" \
@@ -47,30 +62,48 @@ function gh_curl() {
        $@
 }
 
-if [ -z "$FILE" ]; then
-  release_data=`gh_curl -s $GITHUB/repos/$REPO/releases`
-  #echo "release_data: $release_data"
-  asset_id=`echo $release_data | jq ".[0].assets[0].id"`
-  echo "asset_id: $asset_id"
-  FILE=`echo $release_data | jq ".[0].assets[0].name" | tr -d '"'`
-  echo $FILE
-fi;
+release_data=`gh_curl -s $GITHUB/repos/$REPO/releases`
+echo $release_data > release.json
+
 
 if [ "$VERSION" = "latest" ]; then
-  # Github should return the latest release first.
-  parser=".[0].assets | map(select(.name == \"$FILE\"))[0].id"
+  # this has been always the first entry in the array, 
+  # if a wrong version is downloaded, 
+  # check if github changed its response
+  asset_id=`echo $release_data | jq ".[0].assets[0].id"`
+  file_parser=".[0].assets[0].name "
+  
+  # check if file was set from outside, if not, get the original filename:
+  if [ -z $FILE ]; then
+    FILE=$(echo $release_data | jq $file_parser | tr -d '"')
+  fi;
+
 else
-  parser=". | map(select(.tag_name == \"$VERSION\"))[0].assets | map(select(.name == \"$FILE\"))[0].id"
+  #i hade to use jqs --arg to avoid quotes escaping hell here:
+  asset_id=$(echo $release_data | jq --arg ver "$VERSION" ' . | map(select(.tag_name==$ver ))[0].assets[0].id ')
+  echo "asset id found: $asset_id"
+  file_parser=' . | map(select(.tag_name==$ver ))[0].assets[0].name '
+  # check if file was set from outside, if not, get the original filename:
+  if [ -z $FILE ]; then
+    FILE=$(echo $release_data | jq --arg ver "$VERSION" ' . | map(select(.tag_name==$ver ))[0].assets[0].name ' | tr -d '"')
+  fi;
 fi;
 
-
 if [ "$asset_id" = "null" ]; then
-  echo "ERROR: asset id still not set, exiting"
+  echo "ERROR: asset id still not set, cannot download, exiting"
   exit 1
+fi;
+
+echo Starting Download:
+echo "asset_id = $asset_id"
+echo "file=$FILE"
+
+if [ ! -d $DOWNLOAD_DIR ]; then
+  mkdir -p $DOWNLOAD_DIR
 fi;
 
 download_url="https://$TOKEN:@api.github.com/repos/$REPO/releases/assets/$asset_id"
 #actual download
 wget -q --auth-no-challenge --header='Accept:application/octet-stream' \
   $download_url \
-  -O $FILE
+  -O $DOWNLOAD_DIR/$FILE
