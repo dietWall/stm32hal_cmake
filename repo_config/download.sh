d26f38f0e49b8738ff8f651e4d61c22915f8e4ff
Dietrich Wall <dietrich7@gmx.de>
2026-04-20 09:48:47 +0200

    more cleanup

--- repo_config/download.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-
-REPO_ROOT=$(git rev-parse --show-toplevel)
-source $REPO_ROOT/repo_config/.venv_installation/bin/activate
-
-ansible-playbook -c local $REPO_ROOT/repo_config/ansible/download_release.yml --extra-vars "json_file=$REPO_ROOT/repo_config/download_configs/repo_helper.json"
-ansible-playbook -c local $REPO_ROOT/repo_config/ansible/download_release.yml --extra-vars "json_file=$REPO_ROOT/repo_config/download_configs/tcl_utils.json"
--- tests/setup_test_env.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-#python3 -m venv tests/.venv 
-#source tests/.venv/bin/activate 
-#pip install -r requirements.txt
