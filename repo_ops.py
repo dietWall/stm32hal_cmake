31f8f35e246357560f37e0ae44bb2be8e6980782
Dietrich Wall <dietrich7@gmx.de>
2026-04-21 09:37:31 +0200

    container setup is done

--- repo_ops.py
+++ repo_ops.py
@@ -5,19 +5,37 @@ from repo_helper.Repo_Helper import Repo_Helper
 import argparse
 
 MOUNT="/workspace"
-image="ghcr.io/dietwall/arm_gcc_image:latest"
+base_image="ghcr.io/dietwall/arm_gcc_image:latest"
+image="stm32hal_cmake_dev"
 default_container_name = "stm32hal"
 test_venv = ".venv"
+
+def create_container_env(container_name: str = default_container_name):
+    helper = Repo_Helper()
+    _, repo_root = helper.repo_root()
+    command = f"source {MOUNT}/repo_config/.env_container && \
+        {MOUNT}/repo_config/download_release.sh -r dietwall/openocd-tcl-controller -o {MOUNT}/repo_config/tmp_download"
+    
+    result, output = helper.exec_in_container(command=command, container_name=container_name)
+    if result != 0:
+        print(f"Error: {result}")
+        for l in output: print(l.strip())
+    else:
+        print(f"Downloaded release successfully")
 
 def start_container(container_name: str = default_container_name):
     helper = Repo_Helper()
     _, repo_root = helper.repo_root()
+    print(f"starting container: {container_name}")
     command = f"docker run -d -it --name {container_name} --rm --mount type=bind,source={repo_root},dst={MOUNT} {image}"
-    
     print(f"starting container")
     result, output = helper.execute(command)
     print(f"result from execute: {result.returncode}")
-    if result.returncode != 0:
+    if result.returncode == 125:
+        print(f"Returncode: {result.returncode}, is it running?")
+        for f in output:
+            print(f)
+    elif result.returncode != 0:
         print(f"Starting failed with exitcode: {result.returncode}")
         for f in output:
             print(f)
@@ -26,9 +44,26 @@ def start_container(container_name: str = default_container_name):
         print(f"docker exec -it {container_name} bash")
         print(f"to work with it")
     
+    print(f"setting up container environment")
+    create_container_env(container_name)
     helper.create_venv_in_container(container_name=container_name, venv_dir=test_venv)
-    helper.exec_in_container_venv(command="pip install -r requirements.txt", container_name = container_name, venv_path=test_venv)
-    helper.exec_in_container_venv("pip install repo_config/ansible/tmp_install/*.whl", container_name = container_name, venv_path=test_venv)
+    print(f"Installing requirements from {MOUNT}/requirements.txt")
+    helper.exec_in_container_venv(command=f"pip install -r {MOUNT}/requirements.txt", container_name = container_name, venv_path=test_venv)
+    print(f"Installing downloaded dependencies")
+    helper.exec_in_container_venv(command=f"pip install {MOUNT}/repo_config/tmp_download/*.whl", container_name = container_name, venv_path=test_venv)
+
+def build_image(image_name: str = image):
+    print(f"re-building image: {image_name}")
+    helper = Repo_Helper()
+    command = f"docker build -t {image_name} -f .devcontainer/Dockerfile ."
+    print(f"building image")
+    result, output = helper.execute(command)
+    if result.returncode != 0:
+        print(f"Building failed with exitcode: {result.returncode}")
+        for f in output:
+            print(f.strip())
+    else:
+        print(f"Image built successfully")
 
 
 def exec_in_container(command, container_name: str = default_container_name):
@@ -55,7 +90,7 @@ def main():
     parser = argparse.ArgumentParser(description="STM32 HAL CMake repository operations")
     parser.add_argument("--operation", "-o", 
                         help="Operation to perform",
-                        choices=["start_container", "configure", "build", "test_example_compilation", "test_hal_compilation", "test_connection"],
+                        choices=["clean", "start_container", "configure", "build", "test_example_compilation", "test_hal_compilation", "test_connection"],
                         action="append", nargs="+") #required=True
     parser.add_argument("--build_type", "-bt", choices=["Release", "Debug", "RelWithDebInfo", "MinSizeRel"], help="defines the build type", default="Release")
     parser.add_argument("--container", "-c", help="defines a container for the build operation", default=default_container_name)
@@ -66,7 +101,17 @@ def main():
     _, repo_root = helper.repo_root()
     
     if args.operation != None:
+        if "clean" in args.operation[0]:
+            print(f"cleaning up environment, stopping any containers")
+            helper.execute(f"docker stop {args.container}")
+            #make sure it is removed, even if -rm is given
+            helper.execute(f"docker rm {args.container}")
+            for dir in ["build", test_venv, ".venv"]:
+                print(f"removing {repo_root}/{dir}")
+                helper.execute(f"rm -rf {repo_root}/{dir}")
+
         if "start_container" in args.operation[0]:
+            build_image()
             start_container(args.container)
         
         if "configure" in args.operation[0]:
